22432d669644160f0d07ce38278d47e7663984b5
Micael Jerry <fidimalala.mj@gmail.com>
2025-03-31 17:29:16 +0300

    chore: init user ressources

--- prisma/migrations/20250331091805_user/migration.sql
+++ prisma/migrations/20250331091805_user/migration.sql
@@ -3,9 +3,15 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "username" VARCHAR(50) NOT NULL,
     "password" VARCHAR NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "firstname" VARCHAR NOT NULL,
+    "lastname" VARCHAR NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
