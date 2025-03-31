030665c94a2517e9ddba1acaeef4b2516fc30746
Micael Jerry <fidimalala.mj@gmail.com>
2025-03-31 18:45:48 +0300

    docs: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function run() {
 	const port = process.env.PORT ?? 8080;
+
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+
+	const documentBuilderConfig = new DocumentBuilder()
+		.setTitle('Coucou api')
+		.setDescription('Chat app API')
+		.setVersion('0.1')
+		.addTag('chat')
+		.addBasicAuth()
+		.build()
+	const document = SwaggerModule.createDocument(app, documentBuilderConfig, {
+		operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+	});
+	SwaggerModule.setup('api', app, document, {
+		jsonDocumentUrl: 'swagger/json',
+	});
+
 	await app.listen(port);
-	console.info(`Server started on port: ${port}`);
+	console.info(`API is running on: ${await app.getUrl()}`);
 }
 
 void run();
