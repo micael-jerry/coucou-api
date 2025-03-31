68037e155e91a4ebd73d94d2416d9aa33ee14bf5
Micael Jerry <fidimalala.mj@gmail.com>
2025-03-31 18:45:48 +0300

    docs: add swagger docs

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,20 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class SignUpDto {
+	@ApiProperty()
 	@IsNotEmpty()
 	username: string;
 
+	@ApiProperty()
 	@IsEmail()
 	email: string;
 
+	@ApiProperty({
+		minLength: 8,
+		maxLength: 50
+	})
 	@IsNotEmpty()
 	@MinLength(8)
 	@MaxLength(50)
 	password: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	firstname: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	lastname: string;
 }
