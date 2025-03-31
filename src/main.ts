9132b2a4d541b448bdb022803828f52167f02a61
Micael Jerry <fidimalala.mj@gmail.com>
2025-03-31 10:35:16 +0300

    refactor: define folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './module/app.module';
 
 async function run() {
+	const port = process.env.PORT ?? 8080;
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(port);
+	console.info(`Server started on port: ${port}`);
 }
 
 void run();
