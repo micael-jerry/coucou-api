493d10fce0c6316e3fc8e6a416b7f8a1b2a3c928
Micael Jerry <fidimalala.mj@gmail.com>
2025-04-25 15:13:26 +0300

    fix: rename route

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -3,14 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { AuthTokenPayload } from '../payload/auth-token.payload';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 
 export class AuthGuard implements CanActivate {
 	constructor(private readonly jwtService: JwtService) {}
 
 	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-		const ctx = context.switchToHttp();
-		const req: Request = ctx.getRequest<Request>();
-		const res = ctx.getResponse();
+		const httpArgumentHost: HttpArgumentsHost = context.switchToHttp();
+		const req: Request = httpArgumentHost.getRequest<Request>();
+		const res = httpArgumentHost.getResponse();
 
 		try {
 			const authHeader = req.headers.authorization;
