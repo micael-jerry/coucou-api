e69ed2a6f223a58377f99fe09ab71c77e8739684
Micael Jerry <fidimalala.mj@gmail.com>
2025-09-22 17:51:40 +0300

    chore: find user by id

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,6 @@
-import { ApiBadRequestResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Coucou } from './dto/coucou.dto';
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
-import { HttpExceptionResponseDto } from '../exception/dto/http-exception-response.dto';
 import { ApiCommonExceptionsDecorator } from '../exception/decorator/api-common-exceptions.decorator';
 
 @Controller()
@@ -11,7 +10,6 @@ export class HealthController {
 		description: 'Returns a simple message to verify the service is running.',
 	})
 	@ApiResponse({ status: HttpStatus.OK, type: Coucou })
-	@ApiBadRequestResponse({ type: HttpExceptionResponseDto })
 	@ApiCommonExceptionsDecorator()
 	@Get('/coucou')
 	@HttpCode(HttpStatus.OK)
