31bb45e532e2d5f2f70bdda9c78daa99fdeac27d
Micael Jerry <fidimalala.mj@gmail.com>
2025-10-07 13:52:59 +0300

    fix: update role guard

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
@@ -15,17 +15,19 @@ export class RolesGuard implements CanActivate {
 		const httpArgumentsHost: HttpArgumentsHost = context.switchToHttp();
 		const req: Request = httpArgumentsHost.getRequest<Request>();
 
-		if (!this.hasPermission(req.user!, this.getRoles(context)))
-			throw new UnauthorizedException('You do not have the authorization');
+		if (!req.user || !this.hasPermission(req.user, this.getRequiredRoles(context)))
+			throw new ForbiddenException('You do not have the required permissions to access this resource.');
+
 		return true;
 	}
 
-	private getRoles(context: ExecutionContext): UserRole[] {
+	private getRequiredRoles(context: ExecutionContext): UserRole[] | undefined {
 		return this.reflector.get<UserRole[]>(Roles, context.getHandler());
 	}
 
-	private hasPermission(payload: AuthTokenPayload, roles: UserRole[]) {
-		if (roles.includes(payload.user_role)) return true;
+	private hasPermission(payload: AuthTokenPayload, requiredRoles: UserRole[] | undefined) {
+		if (!requiredRoles) return true;
+		if (requiredRoles.includes(payload.user_role)) return true;
 		return false;
 	}
 }
