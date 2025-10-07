dda326f66a3e8e1930e493a0d343676221d1d046
Micael Jerry <fidimalala.mj@gmail.com>
2025-10-07 08:41:21 +0300

    chore: add user role table attribut

---
+++ prisma/migrations/20251007053730_add_user_role/migration.sql
@@ -0,0 +1,6 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER',
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
